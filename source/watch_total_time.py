080e6ecd47e977a1359a1ed0f6923ef7172bc64c
yiweihuang <n1993212@gmail.com>
2017-01-13 17:15:36 +0800

    compute full time of video watching

--- source/watch_total_time.py
+++ source/watch_total_time.py
@@ -8,17 +8,17 @@ video_file = '../video_file/'
 data = {}
 df_ = pd.DataFrame()
 for cid in os.listdir(path):
+    data['cid'] = cid
+    total_cid_time = []
     for chid in os.listdir(path + cid):
         for filename in os.listdir(path + cid + '/' + chid):
             if os.stat(path + cid + '/' + chid + '/' + filename).st_size != 0:
                 df = pd.read_csv(path + cid + '/' + chid + '/' + filename, header=None, usecols=[1,2])
                 total_time = df[1].sub(df[2], axis=0).abs().sum()
-                data['cid'] = cid
-                data['chid'] = chid
-                data['total_time'] = total_time
-                v_df = pd.read_csv(video_file + cid + '/v_chapter_video.csv', header=None, usecols=[4,5])
-                vid = int(filename.split('.')[0])
-                name = v_df.loc[v_df[4] == vid]
-                data['filename'] = name[5]
-                df_ = df_.append(data, ignore_index=True)
+                total_cid_time.append(total_time)
+    sum_time = sum(total_cid_time)
+    m, s = divmod(sum_time, 60)
+    h, m = divmod(m, 60)
+    data['time'] = "%d:%02d:%02d" % (h, m, s)
+    df_ = df_.append(data, ignore_index=True)
 df_.to_csv('out.csv', sep=',', encoding='utf-8')
